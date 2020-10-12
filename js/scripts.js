3f295ab4ea6563307fe4f93603073ec44ff72b9f
Marc Oller <marc.om@icloud.com>
2020-10-12 10:45:10 +0200

    Create loop for Task - Part 1

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,7 @@ let pokemonList = [
   {name: 'Charizard', height: 1.7, types: ['fire', 'flying']},
   {name: 'Butterfree', height: 1.1, types: ['bug', 'flying']}
 ];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' + ' ');
+}
