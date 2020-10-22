690041376713e01ddb40024ad800636876e8b90c
Marc Oller <marc.om@icloud.com>
2020-10-22 10:03:32 +0200

    Add comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+
+// list of pokemons
 let pokemonRepository = (function () {
   let pokemonList = [
     {name: 'Bulbasaur', height: 0.7, types: ['grass', ' poison']},
@@ -7,24 +9,38 @@ let pokemonRepository = (function () {
     {name: 'Butterfree', height: 1.1, types: ['bug', ' flying']}
   ];
 
+  // add item to list
   function add(poke) {
     pokemonList.push(poke);
   }
 
+  // get all items in the list
   function getAll() {
     return pokemonList;
   }
 
-  function addListItem (pokemon) {
+  // selects <ul> and creates <li> childs
+  function addListItem(pokemon) {
     let pokemonList = document.querySelector('.pokemon-list');
     let listItem = document.createElement('li');
+    // creates button for each <li>, add a class and display pokemon name
     let button = document.createElement('button');
     button.innerText = pokemon.name;
     button.classList.add('button-class');
+    // appends and listens to clicks
     listItem.appendChild(button);
     pokemonList.appendChild(listItem);
+    button.addEventListener('click', function(event) {
+      showDetails(pokemon);
+    });
   }
 
+  // displays name in the console
+  function showDetails(pokemon) {
+    console.log(pokemon.name);
+  }
+
+  // returns previous functions
   return {
     add: add,
     getAll: getAll,
@@ -32,9 +48,11 @@ let pokemonRepository = (function () {
   };
 })();
 
+// calls getAll and adds new pokemon
 pokemonRepository.getAll();
 pokemonRepository.add({ name: 'Pikachu' });
 
+// calls addListItem function
 pokemonRepository.getAll().forEach(function (pokemon) {
   pokemonRepository.addListItem(pokemon);
 });
