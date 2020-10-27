27b1925b37338c11f3d544ee69b49a2022107358
Marc Oller <marc.om@icloud.com>
2020-10-27 08:51:41 +0100

    Remove Pokémon objects and create new array

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,15 @@
 
 // list of pokemons
+let pokemonList = []
+
 let pokemonRepository = (function () {
-  let pokemonList = [
-    {name: 'Bulbasaur', height: 0.7, types: ['grass', ' poison']},
-    {name: 'Ivysaur', height: 1, types: ['grass', ' poison']},
-    {name: 'Venusaur', height: 2, types: ['grass', ' poison']},
-    {name: 'Charizard', height: 1.7, types: ['fire', ' flying']},
-    {name: 'Butterfree', height: 1.1, types: ['bug', ' flying']}
-  ];
+  // let pokemonList = [
+  //   {name: 'Bulbasaur', height: 0.7, types: ['grass', ' poison']},
+  //   {name: 'Ivysaur', height: 1, types: ['grass', ' poison']},
+  //   {name: 'Venusaur', height: 2, types: ['grass', ' poison']},
+  //   {name: 'Charizard', height: 1.7, types: ['fire', ' flying']},
+  //   {name: 'Butterfree', height: 1.1, types: ['bug', ' flying']}
+  // ];
 
   // add item to list
   function add(poke) {
